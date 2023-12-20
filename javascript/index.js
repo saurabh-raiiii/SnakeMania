f0825768a5a271c43a4c3d94534fd73ca5118079
Saurabh Rai <51364763+saurabh-raiiii@users.noreply.github.com>
2023-12-20 15:56:57 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -61,7 +61,7 @@ function gameEngine(){
         // generating new food and we have to make sure the food must be not render on the top of the snake
         let a = 2;
         let b = 16;
-        // when a food is render on a snake
+
         for(let i = 0; i< snakeArr.length; i++){
             if(snakeArr[i].x === food.x && snakeArr[i].y === food.y){
                 i = 0;
